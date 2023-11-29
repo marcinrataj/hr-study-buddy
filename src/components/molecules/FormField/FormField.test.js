a4c8e6228c619aa3f3bc16acf920394869f0837e
Marcin Rataj <marcinrataj0@gmail.com>
2023-11-29 19:42:45 +0100

    we start learn testing, testing FormField, AddUser, made other test file component for transfer needed files depper in parent to makes deeper function

---
+++ src/components/molecules/FormField/FormField.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import FormField from './FormField';
+import { renderWithProviders } from 'helpers/renderWithThemeProvider';
+
+describe('Form Field', () => {
+  it('Renders the component', () => {
+    <renderWithProviders>
+      <FormField label="name" name="name" id="name" />
+    </renderWithProviders>;
+  });
+});
